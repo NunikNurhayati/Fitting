6ef2fc6e53db73c30e97b661395ba39de2c58c26
Nunik Nurhayati <Nunik Nurhayati@MSI>
2018-04-20 17:28:32 +0900

    Practice1

---
+++ Practice.py
@@ -0,0 +1,31 @@
+print("I am a chemist.")
+print("She asked, \"What are you doing?\"")
+
+tuna = 5
+carrot = 7
+print(tuna + carrot)
+
+print(5 * 5)
+print(5 ** 3)
+print(18 / 4)
+print(2006500/128)
+print(18//4)
+print(17 + 05)
+print(18/4)
+print(5*3)
+
+print("Why the result of 18/4 is 4 (bilangan bulat)?")
+
+print(2006500./128.)
+
+print("\"Hey, what are you doing?\"")
+
+print("I am studying.\nStudying python from program.")
+
+print(r"I am studying.\nStudying python from program.")
+
+first_name = "Nunik "
+last_name = "Nurhayati"
+full_name = first_name + last_name
+
+print(full_name)
