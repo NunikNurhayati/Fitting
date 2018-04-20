993cd17bef479b714c04eebbb0c6c0e6f572913e
Nunik Nurhayati <Nunik Nurhayati@MSI>
2018-04-20 19:37:05 +0900

    Latihan

--- Practice.py
+++ Practice.py
@@ -15,10 +15,13 @@ print(17 + 05)
 print(18/4)
 print(5*3)
 print("Why the result of 18/4 is 4 (bilangan bulat)?")
+print("--------------------------------------------------")
+
 print(2006500./128.)
 print("\"Hey, what are you doing?\"")
 print("I am studying.\nStudying python from program.")
 print(r"I am studying.\nStudying python from program.")
+print("--------------------------------------------------")
 
 first_name = "Nunik "
 last_name = "Nurhayati"
@@ -34,3 +37,11 @@ print(full_name[6:9])
 print(full_name[:9])
 print(full_name[6:])
 print(full_name[:])
+
+"To know how many characters are inside."
+print(len(full_name))
+print("--------------------------------------------------")
+
+"Numbers"
+players = [17, 99, 83, 60, 59]
+print(players[3])
